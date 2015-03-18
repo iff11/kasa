a1002bd16604b4ceae3565c7606d9bd7b68a78b7
Michal Bryxí <michal.bryxi@gmail.com>
2015-03-19 00:19:40 +0100

    Auto complete attempt

--- frontend/app/components/x-sell/row/form/component.js
+++ frontend/app/components/x-sell/row/form/component.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   tagName: '',
 
   prefill: function() {
+    console.log(this.get('sell.item'));
     this.set('sell.price', this.get('sell.item.selling_price'));
   }.observes('sell.item'),
 });
