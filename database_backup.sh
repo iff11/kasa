2472261294631ca9b2b7833deaeb460808826e8f
Michal Bryxí <michal.bryxi@gmail.com>
2015-03-18 23:03:40 +0100

    Backup script

---
+++ database_backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+now=$(date +"%Y_%m_%d")
+file="/Users/michal/Google Drive/Projekty/topstylesalon.cz/backups/backup_${now}"
+
+#heroku pgbackups:capture
+line=`heroku pg:backups 2> /dev/null | sed -n 4,1p`
+id=`echo $line | awk -F ' ' '{print $1}'`
+url=`heroku pg:backups public-url $id | sed -n 2,1p | awk -F "'" '{print $2}'`
+
+curl -o "$file" "$url"
+du -hs "$file"
