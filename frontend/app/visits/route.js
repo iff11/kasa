e793575d4e3a4a9941eaca433d46ee796ce0634b
Michal Bryxí <michal.bryxi@gmail.com>
2015-03-18 02:00:24 +0100

    Cleanup

--- frontend/app/visits/route.js
+++ frontend/app/visits/route.js
@@ -2,17 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var visits = this.store.find('visit'),
-        employees = this.store.find('employee');
-
-    return Ember.RSVP.hash({
-      visits: visits,
-      employees: employees
-    });
+    return this.store.find('visit');
   },
 
   setupController: function(controller, model) {
-    controller.set('attrs.visits', model.visits);
-    controller.set('attrs.employees', model.employees);
+    controller.set('attrs.visits', model);
   }
 });
