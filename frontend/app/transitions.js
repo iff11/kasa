a2ed1f459974cd39adaa50b1e3d73e8163d29b0d
Michal Bryxí <michal.bryxi@gmail.com>
2015-03-26 16:49:28 +0100

    Animation cleanup

--- frontend/app/transitions.js
+++ frontend/app/transitions.js
@@ -1,37 +1,26 @@
 export default function(){
-  // this.transition(
-  //   this.toRoute('items', 'visits', 'customers'),
-  //   this.use('crossFade')
-  // );
-
   this.transition(
-    this.fromRoute('visits'),
     this.toRoute('visit'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.use('toLeft')
   );
 
   this.transition(
-    this.fromRoute('visit.show'),
+    this.fromRoute('visit'),
+    this.use('toRight')
+  );
+
+  this.transition(
+    this.toRoute('visits'),
+    this.use('toRight')
+  );
+
+  this.transition(
     this.toRoute('visit.checkout'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  );
-
-  // this.transition(
-  //   this.toRoute('visit'),
-  //   this.use('toDown'),
-  //   this.reverse('toUp')
-  // );
-
-  this.transition(
-    this.childOf('.item-count'),
-    this.use('fade')
+    this.use('toLeft')
   );
 
   this.transition(
-    this.toRoute('sell/checkout'),
-    this.use('toDown'),
-    this.reverse('toUp')
+    this.toRoute('visit.show'),
+    this.use('toRight')
   );
 };
