dcec8decea6693b5981d1aeaefb91dd72146450b
Michal Bryxí <michal.bryxi@gmail.com>
2015-04-07 07:32:43 +0200

    Zaměstnancovy zakázky a jeho podíl lze filtrovat podle měsíců

--- frontend/app/admin/employee/controller.js
+++ frontend/app/admin/employee/controller.js
@@ -2,5 +2,41 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   attrs: {},
-  ratio: 0.1
+  ratio: 0.1,
+
+  selectedMonth: null,
+  months: function() {
+    // TODO: start should be decided based on employee created_at
+    var start = moment('2015-01'),
+        end = moment(),
+        months = end.diff(start, 'months'),
+        ret = [],
+        calc = null;
+
+    for(var i=0; i<months; i++) {
+      start.add(1, 'months');
+      ret.push(
+        {
+          text: start.format('MMMM YYYY'),
+          value: start.format('YYYY-MM')
+        }
+      );
+    }
+
+    return ret;
+  }.property(),
+
+  visits: function() {
+    var selected = moment(this.get('selectedMonth.value')),
+        selMonth = selected.month(),
+        selYear = selected.year();
+
+    return this.get('attrs.employee.visits').filter(function(visit) {
+      var then = moment(visit.get('date')),
+          thenMonth = then.month(),
+          thenYear = then.year();
+
+          return thenYear === selYear && thenMonth === selMonth;
+    });
+  }.property('attrs.employee.visits.@each', 'selectedMonth')
 });
--- frontend/app/components/x-visits/employee-share/component.js
+++ frontend/app/components/x-visits/employee-share/component.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  ratio: 1,
-
   sum: function() {
-    return this.get('employee.visits').reduce(function(previousValue, visit){
+    return this.get('visits').reduce(function(previousValue, visit){
         return previousValue + visit.get("employee_share");
       }, 0);
-  }.property('employee.visits.@each.total_price')
+  }.property('visits.@each.total_price')
 });
