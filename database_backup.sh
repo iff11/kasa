246744062921ecaa4473cbc39ef8be2a25e7ac81
Michal Bryxí <michal.bryxi@gmail.com>
2015-04-07 22:07:47 +0200

    Fixed database backup script

--- database_backup.sh
+++ database_backup.sh
@@ -6,7 +6,7 @@ file="/Users/michal/Google Drive/Project/topstylesalon.cz/backups/backup_${now}"
 heroku pg:backups capture
 line=`heroku pg:backups 2> /dev/null | sed -n 4,1p`
 id=`echo $line | awk -F ' ' '{print $1}'`
-url=`heroku pg:backups public-url $id | sed -n 2,1p | awk -F "'" '{print $2}'`
+url=`heroku pg:backups public-url $id | sed -n 1,1p`
 
 curl -o "$file" "$url"
 du -hs "$file"
