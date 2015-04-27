aecbeb8643cc0c3b0dd2d0dc4eb61168f05ea7ac
Michal Bryxí <michal.bryxi@gmail.com>
2015-04-28 00:21:20 +0200

    V naskladneni je zobrazeny margin. Ale neumi zatim reagovat na nakupni cenu.

---
+++ frontend/tests/unit/utils/margin-test.js
@@ -0,0 +1,10 @@
+import margin from '../../../utils/margin';
+import { module, test } from 'qunit';
+
+module('margin');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = margin();
+  assert.ok(result);
+});
