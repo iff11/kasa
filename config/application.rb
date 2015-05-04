5c83b847769a8bce5c282d495345f2ca2e140c7a
Michal Bryxí <michal.bryxi@gmail.com>
2015-05-04 08:39:24 +0200

    We have icons

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Fo
+module App
   class Application < Rails::Application
+    config.assets.enabled = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -18,7 +20,7 @@ module Fo
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :cs
+    # config.i18n.default_locale = :cs
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
