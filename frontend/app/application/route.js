2718e3d8ccd53213129acde0fb1ded2a6c5f17a2
Michal Bryxí <michal.bryxi@gmail.com>
2015-05-07 09:47:53 +0200

    Sidebar menu items finally display counts

--- frontend/app/application/route.js
+++ frontend/app/application/route.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
-  renderTemplate: function() {
-    this.render();
+  // renderTemplate: function() {
+  //   this.render();
 
-    this.render('menu', {
-      outlet: 'menu',
-      controller: 'menu',
-      into: 'application'
-    });
-  }
+  //   this.render('menu', {
+  //     outlet: 'menu',
+  //     // controller: 'menu',
+  //     into: 'application'
+  //   });
+  // }
 });
--- frontend/app/menu/controller.js
+++ frontend/app/menu/controller.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  visits: function() {
+    return this.store.find('visit');
+  }.property(),
+
+  customers: function() {
+    return this.store.find('customer');
+  }.property()
 });
+
