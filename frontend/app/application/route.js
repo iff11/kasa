239b0ed849ddaea0b4975a264142ddf808659a12
Michal Bryxí <michal.bryxi@gmail.com>
2015-08-25 10:05:09 +0200

    Quick fix for login

--- frontend/app/application/route.js
+++ frontend/app/application/route.js
@@ -3,13 +3,16 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   model: function () {
-    return Ember.RSVP.hash({
-      items: this.store.find('item'),
-      visits: this.store.find('visit'),
-      customers: this.store.find('customer'),
-      // sells: this.store.find('sell'),
-      // supplies: this.store.find('supply'),
-      emplyees: this.store.find('employee')
-    });
+    // TODO: Maybe this should be elsewhere? What happens on first page load when user is logged-out, then this is never processed
+    if(this.get('session.isAuthenticated')) {
+      return Ember.RSVP.hash({
+        items: this.store.find('item'),
+        visits: this.store.find('visit'),
+        customers: this.store.find('customer'),
+        // sells: this.store.find('sell'),
+        // supplies: this.store.find('supply'),
+        emplyees: this.store.find('employee')
+      });
+    };
   }
 });
