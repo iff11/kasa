5d98908a10b32b5d263963333befcab8ad5a48e5
Michal Bryxí <michal.bryxi@gmail.com>
2015-09-06 22:17:23 +0200

    Paged item list

--- frontend/app/items/controller.js
+++ frontend/app/items/controller.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.Controller.extend({
   attrs: {},
 
-  sortBy: ['name'],
+  queryParams: ['page', 'perPage'],
 
+  sortBy: ['name'],
   sortedItems: Ember.computed.sort('attrs.items', 'sortBy'),
-  
+
+  page: 1,
+  perPage: 20,
+  pagedItems: pagedArray('sortedItems', {pageBinding: 'page', perPageBinding: 'perPage'}),
+  totalPagesBinding: 'pagedItems.totalPages',
+
   actions: {
     sortBy: function (value) {
       this.set('sortBy', [value]);
