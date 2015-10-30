837f8d594e8c4094ec3c31b7eb6705fc94627c28
Michal Bryxí <michal.bryxi@gmail.com>
2015-10-30 19:21:20 +0100

    Cleaned up and working.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -3,25 +3,28 @@ require 'json'
 class StatusesController < ApplicationController
   include JSONAPI::ActsAsResourceController
 
-  before_action :foo
+  before_action :update_statuses
 
-  def foo
-    query = 'SELECT schemaname,relname,n_live_tup FROM pg_stat_user_tables ORDER BY n_live_tup DESC;'
+  def update_statuses
+    self.update_row_count
+    self.update_table_sizes
+  end
+
+  def update_row_count
+    query = 'SELECT relname AS table, n_live_tup AS rows FROM pg_stat_user_tables ORDER BY n_live_tup DESC;'
     result = ActiveRecord::Base.connection.execute(query)
-    test = Status.where(name: 'test').first_or_initialize
-    test.value = JSON.pretty_generate(result.to_json)
 
+    row = Status.where(name: 'Table row count').first_or_initialize
+    row.value = JSON.pretty_generate(JSON.parse(result.to_json))
+    row.save!
+  end
 
+  def update_table_sizes
+    query = 'SELECT relname as "Table", pg_size_pretty(pg_total_relation_size(relid)) As "Size", pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) as "External Size" FROM pg_catalog.pg_statio_user_tables ORDER BY pg_total_relation_size(relid) DESC;'
+    result = ActiveRecord::Base.connection.execute(query)
 
-
-
-
-
-
-
-
-    
-    # test.value = result.to_json
-    test.save!
+    row = Status.where(name: 'Table sizes').first_or_initialize
+    row.value = JSON.pretty_generate(JSON.parse(result.to_json))
+    row.save!
   end
 end
