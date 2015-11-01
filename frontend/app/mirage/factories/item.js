09b388861d99d089618c8eeeb0d29ee55795d1b5
iff <ivanakozlova@gmail.com>
2015-11-01 22:59:22 +0100

    rozpracovane sortovani, nejsou nadefinoava testovaci data pro v prodeji

--- frontend/app/mirage/factories/item.js
+++ frontend/app/mirage/factories/item.js
@@ -1,6 +1,10 @@
 import Mirage, {faker} from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
-  name() { return faker.commerce.productName(); }
+  //'is-active'() {return faker.list.cycle(true, false);},
+  name(i) { return 'item '+ _.padLeft(i, 3, "0"); },
+  'selling-price'(i) { return 2*i;},
+  bought(i) {return 100-i;},
+  sold(i) {return i;},
   // bought(i) { return faker.number.between(1, 20); }
 });
