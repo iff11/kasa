9406268416ae95f6a1db4ff1353b3ecfc4062c15
Michal Bryxí <michal.bryxi@gmail.com>
2016-02-01 08:13:34 +0100

    Sum

--- frontend/app/components/x-visits/employee-share/component.js
+++ frontend/app/components/x-visits/employee-share/component.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sumEmployeeShareService: function() {
+    return this.getWithDefault('visits', []).reduce(function(previousValue, visit){
+        return previousValue + visit.get("employeeShareService");
+      }, 0);
+  }.property('visits.@each.employeeShareService'),
+
+  sumEmployeeShareSale: function() {
+    return this.getWithDefault('visits', []).reduce(function(previousValue, visit){
+        return previousValue + visit.get("employeeShareSale");
+      }, 0);
+  }.property('visits.@each.employeeShareSale'),
+
   sum: function() {
     return this.getWithDefault('visits', []).reduce(function(previousValue, visit){
         return previousValue + visit.get("employee_share");
