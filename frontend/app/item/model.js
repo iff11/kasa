f87f52a1a2005a45859ff35bc070e00c8d62379a
Michal Bryxí <michal.bryxi@gmail.com>
2016-02-13 19:07:01 +0100

    Removed item description
    As per #35

--- frontend/app/item/model.js
+++ frontend/app/item/model.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  description: DS.attr('string'),
   sellingPrice: DS.attr('number'),
   bought: DS.attr('number'),
   sold: DS.attr('number'),
