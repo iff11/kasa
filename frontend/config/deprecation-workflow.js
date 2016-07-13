0eae6ed0ff106d8531b42f1328e0e156230afe10
Michal Bryxí <michal.bryxi@gmail.com>
2016-07-13 09:03:30 +0100

    Integration of ember-cli-deprecation-workflow

---
+++ frontend/config/deprecation-workflow.js
@@ -0,0 +1,10 @@
+window.deprecationWorkflow = window.deprecationWorkflow || {};
+window.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "silence", matchId: "ember-htmlbars.handlebars-make-bound-helper" },
+    { handler: "silence", matchId: "ember-htmlbars.handlebars-helper" },
+    { handler: "silence", matchId: "ember-application.validate-type" },
+    { handler: "silence", matchMessage: "Ember.keys is deprecated in favor of Object.keys" },
+    { handler: "silence", matchId: "ds.adapter.should-reload-all-default-behavior" }
+  ]
+};
