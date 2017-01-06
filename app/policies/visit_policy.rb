159e101a79efe6387632cdb7473ee1d8dd213afa
Michal Bryxí <michal.bryxi@gmail.com>
2017-01-06 00:20:14 +0000

    #138 concept of company
     - Scoped list of visits

--- app/policies/visit_policy.rb
+++ app/policies/visit_policy.rb
@@ -1,7 +1,12 @@
 class VisitPolicy < ApplicationPolicy
+  def create?
+    # TODO: Check that customer and employee are visible?
+    true
+  end
+
   class Scope < Scope
     def resolve
-      scope
+      @scope.joins(:employee).where(employees: { company_id: @user.company_id })
     end
   end
 end
