d10c32961975af00cedd2e4e8f15f330d81ec78f
Michal Bryxí <michal.bryxi@gmail.com>
2017-01-06 07:37:03 +0000

    #138 concept of company
    - Am able to create new sell in visit

--- app/policies/sell_policy.rb
+++ app/policies/sell_policy.rb
@@ -1,4 +1,17 @@
 class SellPolicy < ApplicationPolicy
+  def create?
+    # TODO: Check that customer and employee are visible?
+    # Problem is that @scope or @record does not give us current data
+    true
+  end
+
+  def update?
+    puts @record.inspect
+    # TODO: Check that customer and employee are visible?
+    # Problem is that @scope or @record does not give us current data
+    true
+  end
+
   class Scope < Scope
     def resolve
       scope
--- app/policies/visit_policy.rb
+++ app/policies/visit_policy.rb
@@ -1,7 +1,12 @@
 class VisitPolicy < ApplicationPolicy
   def create?
     # TODO: Check that customer and employee are visible?
+    # Problem is that @scope or @record does not give us current data
     true
+  end
+
+  def update?
+    @user.company_id == Employee.find(@record.employee_id).company_id
   end
 
   class Scope < Scope
