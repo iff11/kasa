281df27491ad0024fc4efe210fa12b3aad821c30
Michal Bryxí <michal.bryxi@gmail.com>
2017-01-11 20:57:11 +0000

    Fix typo in migrations

--- db/migrate/20170109224021_received_amount_split.rb
+++ db/migrate/20170109224021_received_amount_split.rb
@@ -1,6 +1,6 @@
 class ReceivedAmountSplit < ActiveRecord::Migration
   def change
-    rename_column :visits, :paid_in_cash, :paid_in_cash
+    rename_column :visits, :received_amount, :paid_in_cash
     change_column :visits, :paid_in_cash, :decimal
 
     add_column :visits, :paid_by_card, :decimal, null: false, default: 0.0
