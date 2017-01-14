a6a4b6b94e9dccaf5676697fd433f06562a4ea15
Michal Bryxí <michal.bryxi@gmail.com>
2017-01-14 22:10:02 +0000

    Checkout form completed
    - Form is nice & compact
    - Check for sum of prices
    - Disabling submit button when check does not match

--- frontend/app/visit/checkout/controller.js
+++ frontend/app/visit/checkout/controller.js
@@ -3,8 +3,26 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   attrs: {},
 
-  returnAmount: Ember.computed('attrs.visit.priceWithTip', 'attrs.visit.paidInCash', function() {
-    return this.get('attrs.visit.paidInCash') - this.get('attrs.visit.priceWithTip');
+  returnAmount: Ember.computed('attrs.visit.paidByCard', 'attrs.visit.priceWithTip', 'attrs.visit.paidInCash', function() {
+    let paidInCash = parseInt(this.get('attrs.visit.paidInCash'));
+    let paidByCard = parseInt(this.get('attrs.visit.paidByCard'));
+    let priceWithTip = parseInt(this.get('attrs.visit.priceWithTip'));
+
+    return ((paidInCash + paidByCard) - priceWithTip);
+  }),
+
+  isReturnAmountInvalid: Ember.computed('returnAmount', function () {
+    let returnAmount = this.get('returnAmount');
+    return (returnAmount < 0);
+  }),
+
+  returnAmountClass: Ember.computed('isReturnAmountInvalid', function () {
+    let isReturnAmountInvalid = this.get('isReturnAmountInvalid');
+    if(isReturnAmountInvalid) {
+      return 'has-error';
+    } else {
+      return 'has-success';
+    }
   }),
 
   actions: {
