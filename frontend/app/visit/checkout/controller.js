12ad030d62c778be9cd1d6e2f81ae67a321a2132
Michal Bryxí <michal.bryxi@gmail.com>
2017-01-16 20:03:30 +0000

    #151 Checkout green light fix
    - In checkout the return amount should not be green when individual fields are empty

--- frontend/app/visit/checkout/controller.js
+++ frontend/app/visit/checkout/controller.js
@@ -4,16 +4,20 @@ export default Ember.Controller.extend({
   attrs: {},
 
   returnCash: Ember.computed('attrs.visit.paidByCard', 'attrs.visit.priceWithTip', 'attrs.visit.receivedCash', function() {
-    let receivedCash = parseInt(this.get('attrs.visit.receivedCash'));
-    let paidByCard = parseInt(this.get('attrs.visit.paidByCard'));
-    let priceWithTip = parseInt(this.get('attrs.visit.priceWithTip'));
+    let receivedCash = parseInt(this.get('attrs.visit.receivedCash')) || 0;
+    let paidByCard = parseInt(this.get('attrs.visit.paidByCard')) || 0;
+    let priceWithTip = parseInt(this.get('attrs.visit.priceWithTip')) || 0;
 
     return ((receivedCash + paidByCard) - priceWithTip);
   }),
 
   isReturnCashInvalid: Ember.computed('returnCash', function () {
+    let isAnyEmpty = ['receivedCash', 'paidByCard', 'priceWithTip'].any((item) => {
+      return Ember.isEmpty(this.get(`attrs.visit.${item}`));
+    });
     let returnCash = this.get('returnCash');
-    return (returnCash < 0);
+
+    return (returnCash < 0) || isAnyEmpty;
   }),
 
   returnCashClass: Ember.computed('isReturnCashInvalid', function () {
