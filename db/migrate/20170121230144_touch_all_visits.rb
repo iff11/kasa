9102e8520f77c4d48ab531e1936f84b04d98047a
Michal Bryxí <michal.bryxi@gmail.com>
2017-01-22 00:56:19 +0000

    #54 - initial fill bug fix

--- db/migrate/20170121230144_touch_all_visits.rb
+++ db/migrate/20170121230144_touch_all_visits.rb
@@ -1,7 +1,7 @@
 class TouchAllVisits < ActiveRecord::Migration
   def change
     Visit.all.each do |visit|
-      visit.received_cash += 1
+      visit.received_cash -= 1
       visit.save!
     end
   end
--- db/migrate/20170121230242_add_not_null_constraint_to_sell_entity_id.rb
+++
@@ -1,5 +0,0 @@
-class AddNotNullConstraintToSellEntityId < ActiveRecord::Migration
-  def change
-    # change_column_null :cashbooks, :company_id, false
-  end
-end
