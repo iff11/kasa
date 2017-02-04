b541d828b25f5c45103c36e617e2f1be2c061352
Michal Bryxí <michal.bryxi@gmail.com>
2017-02-04 21:06:53 +0100

    Fixed script

--- script.rb
+++ script.rb
@@ -3,7 +3,9 @@
 results = []
 
 Company.find(2).entities.all.each do |entity|
-  sum = Item.where(entity_id: entity.id).joins(:supplies).sum('purchase_price * quantity')
+  sum = Item.where(entity_id: entity.id).where(unlimited: false).joins(:last_supply).sum('(bought - sold) * purchase_price')
+  # sum = Item.where(entity_id: entity.id).joins(:supplies).sum('purchase_price * quantity')
+  # sum = Item.where(entity_id: entity.id).joins(:supplies).length
   results << { entity_name: entity.name, sum: sum }
 end
 
