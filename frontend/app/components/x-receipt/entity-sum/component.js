57080638be22e9885cca8bbe382a4a55a79b8f07
Michal Bryxí <michal.bryxi@gmail.com>
2017-02-09 17:10:37 +0100

    #191 - ticket printing cleanup

--- frontend/app/components/x-receipt/entity-sum/component.js
+++ frontend/app/components/x-receipt/entity-sum/component.js
@@ -7,22 +7,20 @@ export default Ember.Component.extend({
   sum: Ember.computed('sells.[]', 'entity.id', function () {
     let entityId = this.get('entity.id');
 
-    return DS.PromiseObject.create({
-      promise: this.get('sells').then((sells) => {
-        return Ember.RSVP.all(sells.map(sell => {
-          return sell.get('item').then((item) => {
-            return item.get('entity').then((entity) => {
-              if(entity.get('id') === entityId) {
-                return sell.get('price') * sell.get('count');
-              }
-            })
+    const promise = Ember.RSVP.filter(this.get('sells').toArray(), (sell) => {
+        return sell.get('item').then((item) => {
+          return item.get('entity').then((entity) => {
+            return entity.get('id') === entityId;
           });
-        }));
-      }).then(function(result){
-        return result.reduce((prev, curr) => {
-          return prev + curr;
+        });
+      }).then(function(sellsForCurrentEntity) {
+        return sellsForCurrentEntity.reduce((prev, curr) => {
+          return prev + (curr.get('price') * curr.get('count'));
         }, 0);
-      })
+      });
+
+    return DS.PromiseObject.create({
+      promise: promise
     });
   })
 });
