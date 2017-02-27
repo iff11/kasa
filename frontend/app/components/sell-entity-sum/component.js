919c7f1ab304da6f697dd47b8e727e2ad2109a8f
Michal Bryxí <michal.bryxi@gmail.com>
2017-02-27 10:02:36 +0000

    #202 - cleanup

--- frontend/app/components/sell-entity-sum/component.js
+++ frontend/app/components/sell-entity-sum/component.js
@@ -5,14 +5,9 @@ export default Ember.Component.extend({
   sum: Ember.computed('sells.[]', 'entity.id', function () {
     let entityId = this.get('entity.id');
 
-    console.log('----', entityId, '----');
-
     const promise = Ember.RSVP.filter(this.get('sells').toArray(), (sell) => {
         return sell.get('item').then((item) => {
           return item.get('entity').then((entity) => {
-            if(entity.get('id') === entityId) {
-              console.log(sell.get('price'), sell.get('count'));
-            }
             return entity.get('id') === entityId;
           });
         });
