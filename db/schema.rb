a594c0e3845cf2e8bfe9ae8c1a8077e9176d16e7
Michal Bryxí <michal.bryxi@gmail.com>
2017-03-31 18:41:35 +0100

    #249 entity columns data migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331164704) do
+ActiveRecord::Schema.define(version: 20170331171033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
